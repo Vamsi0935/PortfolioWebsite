bf83b047f27ceb87ca09279b1eb7af5d2a18d650
Vamsi0935 <dvkrishna142000@gmail.com>
2024-08-13 13:05:05 +0530

    changes

--- backend/routes/contact.route.js
+++ backend/routes/contact.route.js
@@ -5,18 +5,10 @@ const Contact = require("../models/contact.model.js");
 router.post("/", async (req, res) => {
   try {
     const { name, email, message } = req.body;
-
-    const newContact = new Contact({
-      name,
-      email,
-      message,
-    });
-
+    const newContact = new Contact({ name, email, message });
     await newContact.save();
-
-    res.status(201).json({ success: true, message: "Message received" });
+    res.json({ success: true });
   } catch (error) {
-    console.error("Error saving contact message:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
