de0bf3e8064520e71b4c4b03b0cccb7638bf0137
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-08-13 12:19:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://portfolio-website-six-mocha.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })
