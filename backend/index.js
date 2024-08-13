e6f10c70badadf7c3de3a616d4a677b24f0d91b9
Vamsi0935 <dvkrishna142000@gmail.com>
2024-08-13 10:52:47 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://dvkrishna142000:S5D9MfV5BvgMRdB3@cluster0.pnf94ly.mongodb.net/MERNProject?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => console.log("MongoDB connected....."))
   .catch((err) => console.error(err));
 
