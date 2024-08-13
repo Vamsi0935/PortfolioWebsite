3ee048af5eddcb9ba2c041d58f305422ade933a5
Vamsi0935 <dvkrishna142000@gmail.com>
2024-08-13 15:49:24 +0530

    changes

--- frontend/src/Components/Skills/Skill.jsx
+++ frontend/src/Components/Skills/Skill.jsx
@@ -5,7 +5,7 @@ import { ContextAPI } from "../../utils/context";
 const Skill = () => {
   const { setRef } = useContext(ContextAPI);
   return (
-    <div className="skills" ref={setRef('Skills')}>
+    <div className="skills" ref={setRef("Skills")}>
       <div className="skills-title">
         <h1>
           Skills <hr style={{ width: "85%" }} />
@@ -14,30 +14,34 @@ const Skill = () => {
       <div className="about-skills">
         <div className="about-skill">
           <p>HTML & CSS</p>
-          <hr style={{ width: "45%" }} />
+          90% &nbsp; <hr style={{ width: "45%" }} />
         </div>
         <div className="about-skill">
           <p>JavaScript</p>
+          75% &nbsp;
           <hr style={{ width: "35%" }} />
         </div>
         <div className="about-skill">
           <p>ReactJs</p>
+          80% &nbsp;
           <hr style={{ width: "40%" }} />
         </div>
         <div className="about-skill">
           <p>NodeJs</p>
-          <hr style={{ width: "38%" }} />
+          78% &nbsp; <hr style={{ width: "38%" }} />
         </div>
         <div className="about-skill">
           <p>Java</p>
+          80% &nbsp;
           <hr style={{ width: "40%" }} />
         </div>
         <div className="about-skill">
           <p>MySQL</p>
-          <hr style={{ width: "45%" }} />
+          90% &nbsp; <hr style={{ width: "45%" }} />
         </div>
         <div className="about-skill">
           <p>MongoDB</p>
+          90% &nbsp;
           <hr style={{ width: "45%" }} />
         </div>
       </div>
